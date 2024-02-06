0b48ec270a75655126fe1c200da0ab374eadf92b
Keiran <teacherkeiran@gmail.com>
2024-02-06 09:37:15 +0000

    Ensure app binding to correct port

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
 
 
 #_____________________________________Configure DB Tables____________________________________________________#
